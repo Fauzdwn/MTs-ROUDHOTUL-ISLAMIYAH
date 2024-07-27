4f934d3c3c478020bb1dd20bf161770a4085a62e
Fauzdwn <akunmlbb.fauzz@gmail.com>
2024-07-27 15:33:19 +0700

    nambahkan animasi loadding baru

--- script.js
+++ script.js
@@ -1,69 +1,85 @@
-      // URL API JSON yang telah Anda buat
-     const apiUrl = 'https://script.googleusercontent.com/macros/echo?user_content_key=ToKBhhttFn92maVI7erGHEy4y6vzI_vLOYZdaT-a3TsAkLlpkOJniDUKIUadKwlGcm_m9EeGqs-vNA-NVQtr7Madz-LE_A_Mm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnCZOpiEnfz5TZfrp2nGU4BEw6uIsROeHHFKro8HWVrV8v9glmAgVfj2DRgTCzrCwLnRa7R_KgPAEt_kCaNoZnK_6LcvpFaYXDQ&lib=MnA_RtjAvg-lWr-OWFsKKlC3xNIJWkbbe';
+document.addEventListener('DOMContentLoaded', function() {
+    const loadingOverlay = document.getElementById('loading-overlay');
+    // Tampilkan overlay putih dan spinner
+    loadingOverlay.style.display = 'flex';
+
+  // URL API JSON yang telah Anda buat
+  const apiUrl = 'https://script.googleusercontent.com/macros/echo?user_content_key=ToKBhhttFn92maVI7erGHEy4y6vzI_vLOYZdaT-a3TsAkLlpkOJniDUKIUadKwlGcm_m9EeGqs-vNA-NVQtr7Madz-LE_A_Mm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnCZOpiEnfz5TZfrp2nGU4BEw6uIsROeHHFKro8HWVrV8v9glmAgVfj2DRgTCzrCwLnRa7R_KgPAEt_kCaNoZnK_6LcvpFaYXDQ&lib=MnA_RtjAvg-lWr-OWFsKKlC3xNIJWkbbe';
        
-     fetch(apiUrl)
-       .then(response => response.json())
-       .then(data => {
-         // Mengambil data dari Sheet1
-         const sitePengaturan = data.SitePengaturan[0];
-       
-         // pengaturan display
-         document.getElementById('nav').style.display = sitePengaturan.DisplayNavbar;
-         document.getElementById('header').style.display= sitePengaturan.DisplayHeader;
-         document.getElementById('body').style.display= sitePengaturan.DisplayBody;
-         document.getElementById('footer').style.display= sitePengaturan.DisplayFooter;
-  
- 
- // pengaturan bawaan
- const pengaturanBawaan = data.PengaturanBawaan[0];
- 
- document.body.style.backgroundColor = pengaturanBawaan.WarnaBackground;
- 
- document.body.style.fontFamily = pengaturanBawaan.NamaFont;
- 
- 
- // navbar
- const navbar = data.Navbar[0];
- 
- document.getElementById('nav').style.backgroundColor = navbar.BackgroundColor;
- document.getElementById('nav').style.position = navbar.StickNavbar;
- document.getElementById('logo').src = navbar.UrlLogo;
- document.getElementById('tekslogo').innerText = navbar.TeksLogo;
- 
- // header
- const header = data.Header[0];
- document.getElementById('judulHeader').innerText = header.JudulBesar;
- document.getElementById('deskripsiHeader').innerText = header.DeskripsiJudul;
- document.getElementById('img1').src = header.img1;
- document.getElementById('img2').src = header.img2;
- document.getElementById('img3').src = header.img3;
- 
- // profil
- const bodyhome = data.BodyHome[0];
- document.getElementById('JudulProfil').innerText = bodyhome.JudulProfil;
- document.getElementById('IsiProfil').innerText = bodyhome.DeskripsiProfil;
- document.getElementById('imgbanner').src = bodyhome.ImgBanner;
- //jumlah bagian
- document.getElementById('jumlahcewek').innerText = bodyhome.Santriwati;
- document.getElementById('jumlahcowok').innerText = bodyhome.Santriwan;
- document.getElementById('jumlahguru').innerText = bodyhome.Guru;
- // konten unggulan
- document.getElementById('JudulKeunggulan1').innerText = bodyhome.JudulKeunggulan1 ;
- document.getElementById('deskripsiunggulan1').innerText = bodyhome.DeskripsiKeunggulan1 ;
- document.getElementById('JudulKeunggulan2').innerText = bodyhome.JudulKeunggulan2 ;
- document.getElementById('deskripsiunggulan2').innerText = bodyhome.DeskripsiKeunggulan2 ;
- document.getElementById('JudulKeunggulan3').innerText = bodyhome.JudulKeunggulan3 ;
- document.getElementById('deskripsiunggulan3').innerText = bodyhome.DeskripsiKeunggulan3 ;
- // kata alumni
- document.getElementById('namaAlumni1').innerText = bodyhome.NamaAlumni1;
- document.getElementById('fotoAlumni1').src = bodyhome.FotoAlumni1;
- document.getElementById('kataAlumni1').innerText = bodyhome.KataAlumni1;
- document.getElementById('namaAlumni2').innerText = bodyhome.NamaAlumni2;
- document.getElementById('fotoAlumni2').src = bodyhome.FotoAlumni2;
- document.getElementById('kataAlumni2').innerText = bodyhome.KataAlumni2;
- document.getElementById('namaAlumni3').innerText = bodyhome.NamaAlumni3;
- document.getElementById('fotoAlumni3').src = bodyhome.FotoAlumni3;
- document.getElementById('kataAlumni3').innerText = bodyhome.KataAlumni3;
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Mengambil data dari Sheet1
+      const sitePengaturan = data.SitePengaturan[0];
+    
+      // pengaturan display
+      document.getElementById('nav').style.display = sitePengaturan.DisplayNavbar;
+      document.getElementById('header').style.display= sitePengaturan.DisplayHeader;
+      document.getElementById('body').style.display= sitePengaturan.DisplayBody;
+      document.getElementById('footer').style.display= sitePengaturan.DisplayFooter;
+
+
+// pengaturan bawaan
+const pengaturanBawaan = data.PengaturanBawaan[0];
+
+document.body.style.backgroundColor = pengaturanBawaan.WarnaBackground;
+
+document.body.style.fontFamily = pengaturanBawaan.NamaFont;
+
+
+// navbar
+const navbar = data.Navbar[0];
+
+document.getElementById('nav').style.backgroundColor = navbar.BackgroundColor;
+document.getElementById('nav').style.position = navbar.StickNavbar;
+document.getElementById('logo').src = navbar.UrlLogo;
+document.getElementById('tekslogo').innerText = navbar.TeksLogo;
+
+// header
+const header = data.Header[0];
+document.getElementById('judulHeader').innerText = header.JudulBesar;
+document.getElementById('deskripsiHeader').innerText = header.DeskripsiJudul;
+document.getElementById('img1').src = header.img1;
+document.getElementById('img2').src = header.img2;
+document.getElementById('img3').src = header.img3;
+
+// profil
+const bodyhome = data.BodyHome[0];
+document.getElementById('JudulProfil').innerText = bodyhome.JudulProfil;
+document.getElementById('IsiProfil').innerText = bodyhome.DeskripsiProfil;
+document.getElementById('imgbanner').src = bodyhome.ImgBanner;
+//jumlah bagian
+document.getElementById('jumlahcewek').innerText = bodyhome.Santriwati;
+document.getElementById('jumlahcowok').innerText = bodyhome.Santriwan;
+document.getElementById('jumlahguru').innerText = bodyhome.Guru;
+// konten unggulan
+document.getElementById('JudulKeunggulan1').innerText = bodyhome.JudulKeunggulan1 ;
+document.getElementById('deskripsiunggulan1').innerText = bodyhome.DeskripsiKeunggulan1 ;
+document.getElementById('JudulKeunggulan2').innerText = bodyhome.JudulKeunggulan2 ;
+document.getElementById('deskripsiunggulan2').innerText = bodyhome.DeskripsiKeunggulan2 ;
+document.getElementById('JudulKeunggulan3').innerText = bodyhome.JudulKeunggulan3 ;
+document.getElementById('deskripsiunggulan3').innerText = bodyhome.DeskripsiKeunggulan3 ;
+// kata alumni
+document.getElementById('namaAlumni1').innerText = bodyhome.NamaAlumni1;
+document.getElementById('fotoAlumni1').src = bodyhome.FotoAlumni1;
+document.getElementById('kataAlumni1').innerText = bodyhome.KataAlumni1;
+document.getElementById('namaAlumni2').innerText = bodyhome.NamaAlumni2;
+document.getElementById('fotoAlumni2').src = bodyhome.FotoAlumni2;
+document.getElementById('kataAlumni2').innerText = bodyhome.KataAlumni2;
+document.getElementById('namaAlumni3').innerText = bodyhome.NamaAlumni3;
+document.getElementById('fotoAlumni3').src = bodyhome.FotoAlumni3;
+document.getElementById('kataAlumni3').innerText = bodyhome.KataAlumni3;
+
+        // Sembunyikan overlay setelah data berhasil dimuat
+        loadingOverlay.style.display = 'none';
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        // Sembunyikan overlay meskipun terjadi error
+        loadingOverlay.style.display = 'none';
+      });
+  });
+    
 
 const menuIcon = document.getElementById('menu-icon');
 const menuList = document.getElementById('menu-list');
