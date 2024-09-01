7b7e25b4c3b619545b5d34af004f4c42e44fef83
zeru-program <justinebogor0609@gmail.com>
2024-09-01 09:02:08 +0700

    add cms

---
+++ dashboard/dashboard.js
@@ -0,0 +1,61 @@
+var db = "https://mtsri-db-default-rtdb.firebaseio.com/";
+
+function save() {
+    fetch(db + "cms/-O5bXQPhmB4JHR2F9w5l.json", {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            titlehead: document.getElementById("title-hd").value,
+            sloganhead: document.getElementById("slogan-hd").value,
+            deskripsihead: document.getElementById("desk-hd").value,
+            ctahead: document.getElementById("cta-hd").value,
+            tentang: document.getElementById("tentang").value,
+            visi: document.getElementById("visi").value,
+            misi: document.getElementById("misi").value,
+            jmlSantriLk: document.getElementById("jumlah-santri-lk").value,
+            jmlSantriPr: document.getElementById("jumlah-santri-pr").value,
+            jmlGuru: document.getElementById("jumlah-guru").value,
+            deskFooter: document.getElementById("desk-footer").value
+        })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            alert("berhasil mengubah konten");
+            location.reload();
+        })
+        .catch(error => console.error(error));
+}
+// fetch cms
+fetch(db + "cms.json")
+    .then(response => response.json())
+    .then(data => {
+        for (let key in data) {
+            var val = data[key];
+            document.getElementById("title-hd").value = val.titlehead;
+            document.getElementById("slogan-hd").value = val.sloganhead;
+            document.getElementById("desk-hd").value = val.deskripsihead;
+            document.getElementById("cta-hd").value = val.ctahead;
+            document.getElementById("tentang").value = val.tentang;
+            document.getElementById("visi").value = val.visi;
+            document.getElementById("misi").value = val.misi;
+            document.getElementById("jumlah-santri-lk").value = val.jmlSantriLk;
+            document.getElementById("jumlah-santri-pr").value = val.jmlSantriPr;
+            document.getElementById("jumlah-guru").value = val.jmlGuru;
+            document.getElementById("desk-footer").value = val.deskFooter;
+        }
+    });
+
+// fetch akun
+fetch(db + "account.json")
+    .then(response => response.json())
+    .then(data => {
+        var length = 0;
+        for (let key in data) {
+            length++;
+        }
+        document.getElementById("fill-acc").innerText = length;
+    });
--- public/script.js
+++ public/script.js
@@ -1,33 +1,47 @@
-document.addEventListener('DOMContentLoaded', () => {
-    var db = "https://mtsri-db-default-rtdb.firebaseio.com/"
+document.addEventListener("DOMContentLoaded", () => {
+    var db = "https://mtsri-db-default-rtdb.firebaseio.com/";
     fetch(db + "cms.json")
-    .then(response => response.json())
-    .then(data => {
-        for (let key in data) {
-            var val = data[key];
-            document.getElementById("judulHeader").textContent = val.titlehead;
-            document.getElementById("sloganHeader").textContent = val.sloganhead;
-            document.getElementById("deskHeader").textContent = val.deskripsihead;
-            document.getElementById("ctaHeader").textContent = val.ctahead;
-            document.getElementById("IsiProfil").textContent = val.tentang
-            document.getElementById("visi").textContent = val.visi
-        }
-    })
-    .catch(error => console.error('Error fetching data:', error));
+        .then(response => response.json())
+        .then(data => {
+            for (let key in data) {
+                var val = data[key];
+                document.getElementById("judulHeader").textContent =
+                    val.titlehead;
+                document.getElementById("sloganHeader").textContent =
+                    val.sloganhead;
+                document.getElementById("deskHeader").textContent =
+                    val.deskripsihead;
+                document.getElementById("ctaHeader").innerHTML =
+                    val.ctahead +
+                    "<span class='bi bi-arrow-right mx-2'></span>";
+                document.getElementById("IsiProfil").textContent = val.tentang;
+                document.getElementById("visi").textContent = val.visi;
+                document.getElementById("misi").textContent = val.misi;
+                document.getElementById("jumlah-santri-pr").textContent =
+                    val.jmlSantriPr;
+                document.getElementById("jumlah-santri-lk").textContent =
+                    val.jmlSantriLk;
+                document.getElementById("jumlah-guru").textContent =
+                    val.jmlGuru;
+                document.getElementById("desk-footer").textContent =
+                    val.deskFooter;
+            }
+        })
+        .catch(error => console.error("Error fetching data:", error));
 });
 
-var isLogin = localStorage.getItem("user")
+var isLogin = localStorage.getItem("user");
 if (isLogin) {
-  document.querySelector(".login-false").style.display = "none"
-  document.querySelector(".login-true").style.display = "flex"
+    document.querySelector(".login-false").style.display = "none";
+    document.querySelector(".login-true").style.display = "flex";
 } else {
-  document.querySelector(".login-false").style.display = "flex"
-  document.querySelector(".login-true").style.display = "none"
+    document.querySelector(".login-false").style.display = "flex";
+    document.querySelector(".login-true").style.display = "none";
 }
 
 function logout() {
-  localStorage.removeItem("user")
-  localStorage.removeItem("pw")
-  alert('success logout')
- location.reload()
+    localStorage.removeItem("user");
+    localStorage.removeItem("pw");
+    alert("success logout");
+    location.reload();
 }
